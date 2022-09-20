07de4d0fa26e500c8857abf7b823c955b5ea8353
Ben1152000 <benjamindarnell00@gmail.com>
2022-09-20 05:18:26 -0500

    Recompile binaries

---
+++ programs/random.asm
@@ -0,0 +1,11 @@
+#each led should receive a random color (with a given brightness)
+
+start:
+#push 0 to stack
+  PUSHZ
+#pick random color
+  
+#set led to color
+#increment index
+#mod index
+#jump to top
