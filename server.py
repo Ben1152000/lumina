fee5f0fb26f30d8c5224588844361676400e24ef
Ben1152000 <benjamindarnell00@gmail.com>
2022-09-26 12:42:10 -0500

    Change color to use assembly

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, abort, render_template
 from flask_restful import Resource, Api
-from program import Program
-from pixels import Pixels
 from multiprocessing import Process, Pipe
 from ctypes import c_bool
 import re, os, subprocess
 
-class ProgramProcess(Process):
+from program import Program
+from pixels import Pixels
 
+
+class ProgramProcess(Process):
     def __init__(self, name, data):
         super().__init__()
         self.pipe_in, self.pipe_out = Pipe()
@@ -21,20 +22,23 @@ class ProgramProcess(Process):
                 self.program = self.pipe_out.recv()
                 assert type(self.program) == Program
 
+
 binaries = {}
-BUILTIN = ['idle', 'rainbow', 'life']
+BUILTIN = ["idle", "rainbow", "life"]
 for name in BUILTIN:
-    with open(f'programs/{name}.bin', 'rb') as binary:
+    with open(f"programs/{name}.bin", "rb") as binary:
         binaries[name] = binary.read()
 
-current = ProgramProcess(name='idle', data=binaries['idle'])
+current = ProgramProcess(name="idle", data=binaries["idle"])
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 class StatusResource(Resource):
     # GET /status/
@@ -42,16 +46,20 @@ class StatusResource(Resource):
     def get(self):
         return f"GET /status"
 
-api.add_resource(StatusResource, '/status')
+
+api.add_resource(StatusResource, "/status")
+
 
 class ProgramsResource(Resource):
     # GET /programs
     # list all programs
     def get(self):
         global binaries
-        return {'programs': list(binaries.keys())}
+        return {"programs": list(binaries.keys())}
 
-api.add_resource(ProgramsResource, '/programs')
+
+api.add_resource(ProgramsResource, "/programs")
+
 
 class ModifyProgramResource(Resource):
     # GET /programs/<name>
@@ -71,7 +79,7 @@ class ModifyProgramResource(Resource):
         if name in BUILTIN:
             abort(403, f"Program {name} cannot be modified.")
         binaries[name] = request.data
-        return '', 204
+        return "", 204
 
     # DELETE /programs/<name>
     # delete a program
@@ -80,9 +88,11 @@ class ModifyProgramResource(Resource):
         if name not in binaries:
             abort(404, "The program does not exist.")
         del binaries[name]
-        return '', 204
+        return "", 204
 
-api.add_resource(ModifyProgramResource, '/programs/<string:name>')
+
+api.add_resource(ModifyProgramResource, "/programs/<string:name>")
+
 
 class ExecuteResource(Resource):
     # GET /execute
@@ -91,7 +101,9 @@ class ExecuteResource(Resource):
         global current
         return current.program.name
 
-api.add_resource(ExecuteResource, '/execute')
+
+api.add_resource(ExecuteResource, "/execute")
+
 
 class ExecuteProgramResource(Resource):
     # POST /execute/<name>
@@ -100,17 +112,15 @@ class ExecuteProgramResource(Resource):
         global binaries, current
         if name not in binaries:
             abort(404, "The program does not exist.")
-        current.pipe_in.send(Program(
-            name=name,
-            data=binaries[name],
-            pixels=Pixels()
-        ))
-        return '', 204
+        current.pipe_in.send(Program(name=name, data=binaries[name], pixels=Pixels()))
+        return "", 204
 
-api.add_resource(ExecuteProgramResource, '/execute/<string:name>')
+
+api.add_resource(ExecuteProgramResource, "/execute/<string:name>")
+
 
 class ColorResource(Resource):
-    color_pattern = re.compile(r'^[0-9a-f]{6}$')
+    color_pattern = re.compile(r"^[0-9a-f]{6}$")
 
     # POST /color/<value>
     # set running program to run solid color
@@ -119,24 +129,36 @@ class ColorResource(Resource):
         if not ColorResource.color_pattern.match(value):
             abort(404, "Invalid color code.")
         value = int(value, 16)
-        r, g, b = ((value >> 16) & 0xff, (value >> 8) & 0xff, value & 0xff)
-        current.pipe_in.send(Program(
-            name='color', data=bytes(
-            [0x31,    r,    g,    b,
-             0x00, 0xe7, 0x11, 0x0a,
-             0xf9, 0x40, 0x06, 0x00]),
-            pixels=Pixels()
-        ))
-        return '', 204
+        r, g, b = ((value >> 16) & 0xFF, (value >> 8) & 0xFF, value & 0xFF)
+        current.pipe_in.send(
+            Program(
+                name="color",
+                data=bytes([
+                    0x31,    r,    g,    b,
+                    0x00, 0xe7, 0x11, 0x0a,
+                    0xf9, 0x40, 0x06, 0x00
+                ]),
+                pixels=Pixels(),
+            )
+        )
+        return "", 204
 
-api.add_resource(ColorResource, '/color/<string:value>')
 
-if __name__ == '__main__':
+api.add_resource(ColorResource, "/color/<string:value>")
+
+if __name__ == "__main__":
     current.start()
     app.secret_key = os.urandom(12)
     try:
-        host = subprocess.check_output(['/bin/hostname', '-I']).split()[0].strip().decode('utf-8')
+        host = (
+            subprocess.check_output(["/bin/hostname", "-I"])
+            .split()[0]
+            .strip()
+            .decode("utf-8")
+        )
     except IndexError:
-        raise Exception(subprocess.check_output(['/bin/hostname', '-I']))
-    print("Host:", host) # Right now this breaks under Darnell wifi
-    app.run(debug=False, use_reloader=False, host=host, port=8010) # change use_reloader to True when running
+        raise Exception(subprocess.check_output(["/bin/hostname", "-I"]))
+    print("Host:", host)  # Right now this breaks under Darnell wifi
+    app.run(
+        debug=False, use_reloader=False, host=host, port=8010
+    )  # change use_reloader to True when running
